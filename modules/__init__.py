403b6c1414a5a829995d006bcff30f92cf74aefb
kublitsky90-eng <kublitsky90@gmail.com>
2026-04-29 13:45:07 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,5 +5,7 @@ from . import guild
 from . import scheduler
 from . import utils
 from . import data_handlers
+from . import guild_characters
 
 from .guild import start, help_command, get_guild, commands_list_command
+from .guild_characters import unit_command, unit_info_command
